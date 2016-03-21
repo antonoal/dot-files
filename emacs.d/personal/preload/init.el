d312f001a7fd4aee490cb08ca8fd4bc2ecafa353
Alex Antonov <alexandr.antonov@barclyas.com>
2016-03-21 17:12:46 +0000

    proxy

--- emacs.d/personal/preload/init.el
+++ emacs.d/personal/preload/init.el
@@ -4,6 +4,19 @@
 
 ;;; Code:
 
+;(setq url-proxy-services
+;   '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+;     ("http" . "primary-proxy.gslb.intranet.barcapint.com:8080")
+;     ("https" . "primary-proxy.gslb.intranet.barcapint.com:8080")))
+
+;(setq url-http-proxy-basic-auth-storage
+;    (list (list "primary-proxy.gslb.intranet.barcapint.com:8080"
+;                (cons "Input your LDAP UID !"
+;                      (base64-encode-string "antonova:XXXXXXX")))))
+
+;; Disabling default prelude theme
+(setq prelude-theme nil)
+
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
