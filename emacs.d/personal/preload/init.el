e5bfe6389f8a6f822880bedaec11d7199b593735
Alex Antonov <alexandr.antonov@barclyas.com>
2016-03-24 09:03:53 +0000

    Added tmux conf

--- emacs.d/personal/preload/init.el
+++ emacs.d/personal/preload/init.el
@@ -4,6 +4,7 @@
 
 ;;; Code:
 
+;;Proxy
 ;(setq url-proxy-services
 ;   '(("no_proxy" . "^\\(localhost\\|10.*\\)")
 ;     ("http" . "primary-proxy.gslb.intranet.barcapint.com:8080")
@@ -17,6 +18,7 @@
 ;; Disabling default prelude theme
 (setq prelude-theme nil)
 
+;; Adding additional repos to package manager
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
